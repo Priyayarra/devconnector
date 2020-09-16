862c2ce3359341f8f5eb1b7e92e9eb5d362a9756
priyayarra <priyanka@signalx.ai>
2020-09-16 13:24:25 +0530

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -169,4 +169,67 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+    '/experience', 
+    [ 
+        auth,
+        [
+            check('title', 'Title is required')
+                .not()
+                .isEmpty(),
+            check('company', 'Company is required')
+                .not()
+                .isEmpty(),
+            check('from', 'From date is required')
+                .not()
+                .isEmpty()
+        ] 
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            console.log(profile);
+            profile.experience.unshift(newExp);
+
+            await profile.save();
+
+            console.log(profile);
+            
+
+            res.json(profile);
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
 module.exports = router;
